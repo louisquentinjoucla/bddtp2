b01892c7ee5a25842ec8e57a5c93b374dff45c75
lowlighter <simon.lecoq@live.fr>
2019-04-04 18:11:29 -0400

    Added websockets support

--- build.sbt
+++ build.sbt
@@ -4,7 +4,21 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+lazy val akkaVersion = "2.5.22"
+lazy val akkaHttpVersion = "10.0.9"
+
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.4.0",
-  "org.apache.spark" % "spark-sql_2.12" % "2.4.0"
+  "org.apache.spark" % "spark-sql_2.12" % "2.4.0",
+  // akka
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  // streams
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  // akka http
+  "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion,
 )
+
--- src/main/scala/Exercise1.scala
+++ src/main/scala/Exercise1.scala
@@ -1,5 +1,12 @@
 //Imports
 import org.apache.spark.sql.SparkSession
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.scaladsl.Flow
+import akka.http.scaladsl.server.Route
 
 //Source
 object Exercise1 extends App {
@@ -22,8 +29,27 @@ object Exercise1 extends App {
     .flatMap{case (monster, spells) => spells.map(spell => (spell, monster))}
     .groupByKey()
 
-  //DEBUG
-    bv_spells_monsters
-      .take(30)
-      .foreach(println)
+  //Setup WebSockets server with akka
+  implicit val system = ActorSystem("akka-system")
+  implicit val materializer = ActorMaterializer()
+  implicit val execution = system.dispatcher
+  val route = MainService.route ~ QueryService.route
+
+  //Start server
+  val binding = Http().bindAndHandle(route, "localhost", 8080)
+  println("Server is online")
+  //binding.flatMap(_.unbind()).onComplete(_ => system.terminate())
+  //println("Server is offline")
+}
+
+//WebServices
+trait WebService { def route: Route }
+//Default service
+object MainService extends WebService { override def route: Route = pathEndOrSingleSlash { complete("Welcome to websocket server") } }
+//Query service
+object QueryService extends WebService { override def route: Route = path("q") { get { handleWebSocketMessages(service) } }
+  val service: Flow[Message, Message, _] = Flow[Message].map {
+    case TextMessage.Strict(txt) => TextMessage("ECHO: " + txt)
+    case _ => TextMessage("Message type unsupported")
+  }
 }
